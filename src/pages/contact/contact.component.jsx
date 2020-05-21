bdae8c0de708fc7dfbf58a0ca22d3872e3a12220
Ashutosh Dwivedi <starcdwivedi@gmail.com>
2020-05-21 16:55:44 +0530

    animation effect added in contact page

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -12,13 +12,16 @@ import {
 
 const ContactPage = () => (
   <ContactPageContainer>
-    <ContactUs>CONTACT US</ContactUs>
+    <ContactUs className="animate__animated animate__rubberBand">
+      CONTACT US
+    </ContactUs>
     <Heading>HEAD OFFICE</Heading>
     <HeadofficeSection>
-      <HeadImageContainer>
+      <HeadImageContainer className="animate__animated animate__fadeInLeft">
         <Headoffice />
       </HeadImageContainer>
-      <HeadTextContainer>
+
+      <HeadTextContainer className="animate__animated animate__fadeInRight">
         There are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration in some form, by injected humour, or
         randomised words which don't look even slightly believable. If you are
@@ -29,6 +32,22 @@ const ContactPage = () => (
         a dictionary of over 200 Latin words, combined with a handful of model
         sentence structures, to generate Lorem Ipsum which looks reasonable.
       </HeadTextContainer>
+    </HeadofficeSection>
+    <HeadofficeSection>
+      <HeadTextContainer data-aos="fade-right">
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humour, or
+        randomised words which don't look even slightly believable. If you are
+        going to use a passage of Lorem Ipsum, you need to be sure there isn't
+        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+        generators on the Internet tend to repeat predefined chunks as
+        necessary, making this the first true generator on the Internet. It uses
+        a dictionary of over 200 Latin words, combined with a handful of model
+        sentence structures, to generate Lorem Ipsum which looks reasonable.
+      </HeadTextContainer>
+      <HeadImageContainer data-aos="fade-left">
+        <Headoffice />
+      </HeadImageContainer>
     </HeadofficeSection>
   </ContactPageContainer>
 );
